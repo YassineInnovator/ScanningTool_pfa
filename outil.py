0e0a7d1c2ed91a20e6ee240cda440e1dee49bf53
Khalid06j <100048931+Khalid06j@users.noreply.github.com>
2024-03-24 00:00:24 +0000

    Update outil.py

--- outil.py
+++ outil.py
@@ -1,8 +1,10 @@
 import nmap # type: ignore
 import os
 import socket
+import scapy.all as scapy
+import argparse
 
-sc = nmap.PortScanner()
+'''sc = nmap.PortScanner()
 
 
 #----------------------------------------------
@@ -76,8 +78,33 @@ def nmap():
 
 def vuln():
     ip = input("\nEnter the IP address you want to scan:")  # Enter the IP address you want to scan
-    print(os.system('nmap -sV --script=vulscan.nse ' +ip))
+    print(os.system('nmap -sV --script=vulscan.nse ' +ip))'''
+
+#Network Scanner in Python : 
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Sepcify target ip or ip range")
+    options = parser.parse_args()
+    return  options
+
+def scan(ip):
+    arp_packet = scapy.ARP(pdst=ip)
+    broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_broadcast_packet = broadcast_packet/arp_packet
+    answered_list = scapy.srp(arp_broadcast_packet, timeout=1, verbose=False)[0]
+    client_list = []
+
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        client_list.append(client_dict)
+
+    return client_list
+
+def print_result(scan_list):
+    print("IP\t\t\tMAC\n----------------------------------------")
+    for client in scan_list:
+        print(client["ip"] + "\t\t" + client["mac"])
 
 if __name__=='__main__':
     #print("""Outil de scan""")
-    main()
+    #main()
